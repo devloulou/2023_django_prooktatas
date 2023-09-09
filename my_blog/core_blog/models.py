4dc28a946409a7261d7a4a9d75d68ec1224ce0da
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-09-09 13:52:27 +0200

    first views implementation

--- my_blog/core_blog/models.py
+++ my_blog/core_blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+"""
+Model lefejlesztve -> python manage.py makemigrations -> python manage.py migrate
+"""
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=4000)
+    date_posted = models.DateTimeField(default=timezone.now) #referencia hivatkozás
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'posts'
+
+    def __str__(self):
+        return f"{self.author}_{self.title.split(' ')[0]}"
