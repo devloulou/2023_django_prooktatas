af004c7b0a9440e3384ed2b5b38a55201a01564f
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-09-09 16:57:47 +0200

    implement base structure

--- my_blog/core_blog/views.py
+++ my_blog/core_blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import PostModel
+from django.contrib.auth.models import User
+
 """
 A View fogja renderelni a HTML file-okat és odaadni a böngészőnek
 """
@@ -12,11 +14,24 @@ Később class-based viewkat
 """
 """
 View -> ki kell vezetni url-re ->a fő url-be behivatkozom
+
+Tipikusan a web backend fejlesztésnél:
+CRUD - Create Read Update Delete
+     - Insert Select Update Delete - db utasítás
+     - Post Get Put Delete - HTTP request type
+
 """
 
 
 def index(request):
-    return HttpResponse("Üdv a Django kurzuson")
+    context = {
+        'posts': PostModel.objects.all() # select * from posts
+    }
+
+    # test = PostModel.objects.create(title="Manual data", content="Ez egy test", author=User.objects.get(id=1))
+    # test.save()
+
+    return render(request, 'blog/index.html', context=context)
 
 def test_view(request):
     return HttpResponse("Test oldalon vagyok")
